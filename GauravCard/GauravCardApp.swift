ef05c7d8020160ca2e618c6083d254aa277e0dea
Gaurav <gauravpatil24121997@gmail.com>
2024-01-26 19:10:35 -0800

    Initial Commit

---
+++ GauravCard/GauravCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  GauravCardApp.swift
+//  GauravCard
+//
+//  Created by Gaurav Patil on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct GauravCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
